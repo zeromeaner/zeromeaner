69b26302fa26f260b81197e78e91cc3f5e84491d
robin <robin@robinkirkman.com>
2012-11-25 22:35:49 -0500

    Delete ancient files

--- play_sdl.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=lib;%PATH%;%systemroot%\SysWOW64
-start javaw -cp bin;NullpoMino.jar;lib\log4j-1.2.15.jar;lib\sdljava.jar -Djava.library.path=lib mu.nu.nullpo.gui.sdl.NullpoMinoSDL
--- ruleeditor.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set path=%path%;%systemroot%\SysWOW64
-start javaw -cp bin;NullpoMino.jar;lib\log4j-1.2.15.jar mu.nu.nullpo.tool.ruleeditor.RuleEditor
