38f329d7e26fb6773e0497b680474ff8a9fc97ad
robin <robin@robinkirkman.com>
2012-11-28 04:12:44 -0500

    Start adding eviline to zerom

---
+++ src/main/java/org/zeromeaner/game/evil/TNConcurrentAngelRandomizer.java
@@ -0,0 +1,33 @@
+package org.zeromeaner.game.evil;
+
+import org.zeromeaner.game.play.GameEngine;
+
+import org.eviline.Shape;
+import org.eviline.randomizer.AngelRandomizer;
+import org.eviline.randomizer.ConcurrentRandomizer;
+import org.eviline.randomizer.ThreadedMaliciousRandomizer;
+import org.eviline.randomizer.MaliciousRandomizer.MaliciousRandomizerProperties;
+
+public class TNConcurrentAngelRandomizer extends TNRandomizer {
+	@Override
+	public void setEngine(GameEngine engine) {
+		super.setEngine(engine);
+		MaliciousRandomizerProperties mp = new MaliciousRandomizerProperties(3, .01, true, 15);
+		AngelRandomizer t = new AngelRandomizer(mp);
+		t.setRandom(engine.random);
+		field.setProvider(new ConcurrentRandomizer(t));
+	}
+	
+	@Override
+	public String getName() {
+		return "FAST ANGELIC";
+	}
+	
+	@Override
+	public synchronized int next() {
+		if(regenerate)
+			field.setShape(Shape.O_DOWN);
+		return super.next();
+	}
+
+}
