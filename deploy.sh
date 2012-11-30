153ddf8e00a056d57c52ec0ed69096c24cc4ae4f
robin <robin@robinkirkman.com>
2012-11-30 01:23:06 -0500

    Ability to support dev builds.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 mvn clean install
 mvn -P applet dependency:copy-dependencies package
 mvn -P shade package
-scp target/zeromeaner.jar www.zeromeaner.org:/var/www/zeromeaner/zeromeaner.jar
-scp target/zeromeaner-all-*.jar www.zeromeaner.org:/var/www/zeromeaner/builds
-scp target/dependency/*.jar www.zeromeaner.org:/var/www/zeromeaner/
+scp target/zeromeaner.jar www.zeromeaner.org:/var/www/zeromeaner/{$DEV}/zeromeaner.jar
+scp target/zeromeaner-all-*.jar www.zeromeaner.org:/var/www/zeromeaner/{$DEV}/builds
+scp target/dependency/*.jar www.zeromeaner.org:/var/www/zeromeaner/{$DEV}/
 scp -r src/main/site/* www.zeromeaner.org:/var/www/zeromeaner/
