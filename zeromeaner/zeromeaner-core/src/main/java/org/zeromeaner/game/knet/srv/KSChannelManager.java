948c07581a1c5d6818325b57399e6149e3b4f1aa
Robin Kirkman <robin@robinkirkman.com>
2013-01-29 02:58:38 -0500

    CHANNEL_LEAVE command

--- zeromeaner/zeromeaner-core/src/main/java/org/zeromeaner/game/knet/srv/KSChannelManager.java
+++ zeromeaner/zeromeaner-core/src/main/java/org/zeromeaner/game/knet/srv/KSChannelManager.java
@@ -141,6 +141,27 @@ public class KSChannelManager extends KNetClient implements KNetListener {
 					CHANNEL_ID, id,
 					PAYLOAD, info);
 		}
+		if(e.is(CHANNEL_LEAVE) && e.is(CHANNEL_ID)) {
+			int id = (Integer) e.get(CHANNEL_ID);
+			if(!channels.containsKey(id)) {
+				client.reply(e, ERROR, "Unknown channel id " + id);
+				return;
+			}
+			ChannelInfo info = channels.get(id);
+			if(lobby.equals(info)) {
+				client.reply(e, ERROR, "Cannot leave lobby");
+				return;
+			}
+			if(!info.getMembers().contains(e.getSource())) {
+				client.reply(e, ERROR, "Not in channel with id " + id);
+				return;
+			}
+			info.getMembers().remove(e.getSource());
+			client.reply(e,
+					CHANNEL_LEAVE, true,
+					CHANNEL_ID, id,
+					PAYLOAD, info);
+		}
 	}
 
 }
