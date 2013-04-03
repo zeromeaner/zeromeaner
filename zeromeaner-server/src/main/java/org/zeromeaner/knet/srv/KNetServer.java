101e66f8bc8ec78c7d56b7a438e26589ce7e83b0
Robin Kirkman <robin@robinkirkman.com>
2013-04-03 05:35:14 -0400

    Massively increase the write buffer

--- zeromeaner-server/src/main/java/org/zeromeaner/knet/srv/KNetServer.java
+++ zeromeaner-server/src/main/java/org/zeromeaner/knet/srv/KNetServer.java
@@ -131,7 +131,7 @@ public class KNetServer {
 	public KNetServer(int port) throws IOException, InterruptedException {
 		this.port = port;
 		source = new KNetEventSource(nextClientId.incrementAndGet());
-		server = new Server(1024 * 16, 1024 * 256);
+		server = new Server(1024 * 256, 1024 * 256);
 		KNetKryo.configure(server.getKryo());
 		server.start();
 		server.bind(port, port);
