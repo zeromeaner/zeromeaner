eaac59e29888907648713344c3ed7143308653c7
Robin Kirkman <robin@robinkirkman.com>
2013-04-10 19:54:35 -0400

    Fix buffer usage

--- zeromeaner-core/src/main/java/org/zeromeaner/gui/common/DocumentOutputStream.java
+++ zeromeaner-core/src/main/java/org/zeromeaner/gui/common/DocumentOutputStream.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
+import org.zeromeaner.util.EQInvoker;
+
 public class DocumentOutputStream extends OutputStream {
 	private Document doc;
 	
@@ -15,6 +17,8 @@ public class DocumentOutputStream extends OutputStream {
 	
 	@Override
 	public void write(int b) throws IOException {
+		if(EQInvoker.reinvoke(false, this, b))
+			return;
 		try {
 			doc.insertString(doc.getLength(), String.valueOf((char) (b & 0xff)), null);
 		} catch(BadLocationException ble) {
@@ -23,9 +27,12 @@ public class DocumentOutputStream extends OutputStream {
 
 	@Override
 	public void write(byte[] b, int off, int len) throws IOException {
+		if(EQInvoker.reinvoke(false, this, b, off, len))
+			return;
 		StringBuilder sb = new StringBuilder();
-		for(int i : b) {
-			sb.append((char)(i & 0xff));
+		for(int i = off; i < off + len; i++) {
+			int c = b[i];
+			sb.append((char)(c & 0xff));
 		}
 		try {
 			doc.insertString(doc.getLength(), sb.toString(), null);
