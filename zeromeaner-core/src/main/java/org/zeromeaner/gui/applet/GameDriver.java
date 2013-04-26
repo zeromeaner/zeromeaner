a55da45b6b031ae3f2445a72630378cd3cf9f87e
Robin Kirkman <robin@robinkirkman.com>
2013-04-25 21:41:50 -0400

    More refactoring to support reskin.

--- zeromeaner-core/src/main/java/org/zeromeaner/gui/applet/GameDriver.java
+++ zeromeaner-core/src/main/java/org/zeromeaner/gui/applet/GameDriver.java
@@ -3,6 +3,8 @@ package org.zeromeaner.gui.applet;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.EventListener;
+import java.util.EventObject;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,16 +15,42 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import javax.swing.event.EventListenerList;
+
 import org.apache.log4j.Logger;
 import org.zeromeaner.game.play.GameManager;
 import org.zeromeaner.gui.applet.FrameRateCounter.Frame;
 import org.zeromeaner.util.Threads;
 
 public class GameDriver implements KeyListener, Callable<Integer> {
-	public static interface GameDriverView {
-		public void renderGame(GameDriver driver);
-		public void addKeyListener(KeyListener l);
-		public void removeKeyListener(KeyListener l);
+
+	public static class GameDriverEvent extends EventObject {
+		private int visibleFrameRate;
+		private int totalFrameRate;
+		
+		public GameDriverEvent(GameDriver source) {
+			super(source);
+			visibleFrameRate = source.visibleFrames.rate();
+			totalFrameRate = source.totalFrames.rate();
+		}
+		
+		@Override
+		public GameDriver getSource() {
+			return (GameDriver) super.getSource();
+		}
+		
+		public int getVisibleFrameRate() {
+			return visibleFrameRate;
+		}
+		
+		public int getTotalFrameRate() {
+			return totalFrameRate;
+		}
+	}
+	
+	public static interface GameDriverListener extends EventListener {
+		public void frameRendered(GameDriverEvent e);
+		public void frameSkipped(GameDriverEvent e);
 	}
 	
 	private static final Logger log = Logger.getLogger(GameDriver.class);
@@ -64,27 +92,23 @@ public class GameDriver implements KeyListener, Callable<Integer> {
 	private Future<?> driverFuture;
 	
 	protected GameManager manager;
-	protected GameDriverView view;
+
+	protected EventListenerList listenerList = new EventListenerList();
 	
-	public GameDriver(GameManager manager, GameDriverView view) {
+	public GameDriver(GameManager manager) {
 		this.manager = manager;
-		this.view = view;
-	}
-	
-	public GameDriverView getView() {
-		return view;
-	}
-	
-	public void setView(GameDriverView view) {
-		if(this.view != null)
-			this.view.removeKeyListener(this);
-		this.view = view;
-		if(this.view != null)
-			this.view.addKeyListener(this);
 	}
 	
 	public GameManager getManager() {
 		return manager;
+	}
+	
+	public void addGameDriverListener(GameDriverListener l) {
+		listenerList.add(GameDriverListener.class, l);
+	}
+	
+	public void removeGameDriverListener(GameDriverListener l) {
+		listenerList.remove(GameDriverListener.class, l);
 	}
 	
 	public synchronized void start() {
@@ -123,14 +147,12 @@ public class GameDriver implements KeyListener, Callable<Integer> {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		setButtonPressedState(e.getKeyCode(), true);
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		setButtonPressedState(e.getKeyCode(), false);
 	}
 	
 	private void setButtonPressedState(int keyCode, boolean pressed) {
@@ -147,12 +169,37 @@ public class GameDriver implements KeyListener, Callable<Integer> {
 		}
 	}
 	
+	protected void fireFrameRendered() {
+		Object[] ll = listenerList.getListenerList();
+		GameDriverEvent e = null;
+		for(int i = ll.length - 2; i >= 0; i -= 2) {
+			if(ll[i] == GameDriverListener.class) {
+				if(e == null)
+					e = new GameDriverEvent(this);
+				((GameDriverListener) ll[i+1]).frameRendered(e);
+			}
+		}
+	}
+	
+	protected void fireFrameSkipped() {
+		Object[] ll = listenerList.getListenerList();
+		GameDriverEvent e = null;
+		for(int i = ll.length - 2; i >= 0; i -= 2) {
+			if(ll[i] == GameDriverListener.class) {
+				if(e == null)
+					e = new GameDriverEvent(this);
+				((GameDriverListener) ll[i+1]).frameSkipped(e);
+			}
+		}
+	}
+	
 	private synchronized void tick(boolean render) {
 		update();
-		if(render && view != null) {
-			view.renderGame(this);
+		if(render) {
 			render();
-		}
+			fireFrameRendered();
+		} else
+			fireFrameSkipped();
 	}
 	
 	protected void update() {
