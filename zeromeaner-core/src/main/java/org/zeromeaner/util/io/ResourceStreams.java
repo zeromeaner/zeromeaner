42f9381240544ea3e7bfe93c039062559e6bf6ef
robin <robin.kirkman@gmail.com>
2014-12-06 23:02:39 -0500

    ResourceStreamHook + classpath hook

---
+++ zeromeaner-core/src/main/java/org/zeromeaner/util/io/ResourceStreams.java
@@ -0,0 +1,50 @@
+package org.zeromeaner.util.io;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+import org.zeromeaner.util.ServiceHookDispatcher;
+
+public class ResourceStreams {
+	
+	private static final Logger log = Logger.getLogger(ResourceStreams.class);
+	
+	private static ResourceStreams instance = new ResourceStreams();
+	public static ResourceStreams get() {
+		return instance;
+	}
+	
+	private ServiceHookDispatcher<ResourceStreamHook> hook;
+	
+	private ResourceStreams() {
+		hook = new ServiceHookDispatcher<>(ResourceStreamHook.class);
+	}
+	
+	public InputStream inputStream(String resource) {
+		PrioritizedHandler<Callable<InputStream>> handlers = new PrioritizedHandler<>();
+		hook.dispatcher().addInputHandler(resource, handlers);
+		for(Callable<InputStream> handler : handlers.get()) {
+			try {
+				return handler.call();
+			} catch(Exception e) {
+				log.warn(e);
+			}
+		}
+		return null;
+	}
+	
+	public OutputStream outputStream(String resource) {
+		PrioritizedHandler<Callable<OutputStream>> handlers = new PrioritizedHandler<>();
+		hook.dispatcher().addOutputHandler(resource, handlers);
+		for(Callable<OutputStream> handler : handlers.get()) {
+			try {
+				return handler.call();
+			} catch(Exception e) {
+				log.warn(e);
+			}
+		}
+		return null;
+	}
+}
