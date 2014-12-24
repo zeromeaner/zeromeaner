d9adf043e5a1e8ac06b0d2bb6fb0a20421de15c5
robin <robin.kirkman@gmail.com>
2014-12-23 21:40:38 -0500

    The start of a script to do the reversioning per branch name

---
+++ zeromeaner-parent/reversion.sh
@@ -0,0 +1,24 @@
+BRANCH=$(git symbolic-ref --short HEAD | xargs echo -n)
+
+case $BRANCH in
+master)
+	VSFX=""
+	;;
+*/ZRM-[0-9]*-*)
+	VSFX=$(echo -n $BRANCH | perl -p -e 's/^.*(ZRM-[0-9]+)-.*$/-$1/')
+	;;
+*)
+	VSFX=$(echo -n "-${BRANCH}" | perl -p -e 's/\W/-/g')
+	;;
+esac
+
+VERSION=$(mvn -q \
+     -Dexec.executable="echo" \
+     -Dexec.args='${project.version}' \
+     --non-recursive \
+     org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | xargs echo -n)
+
+NEW_VERSION=$(echo $VERSION | perl -p -e "s/-SNAPSHOT/${VSFX}-SNAPSHOT/")
+
+echo ${NEW_VERSION}
+
