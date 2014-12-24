32758c1e0f30abb050102ee7b81952d46f4fdcbc
robin <robin.kirkman@gmail.com>
2014-12-24 17:42:44 -0500

    Fix clicking on osx

--- zeromeaner-core/src/main/java/org/zeromeaner/sound/SampleBufferClippish.java
+++ zeromeaner-core/src/main/java/org/zeromeaner/sound/SampleBufferClippish.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 import org.apache.commons.io.IOUtils;
@@ -13,18 +14,11 @@ import org.zeromeaner.gui.reskin.StandaloneResourceHolder;
 
 public class SampleBufferClippish extends Thread {
 	
-	private static final SampleBuffer silence;
+	private static final byte[] silence;
 	static {
-		try {
-			AudioInputStream ain = AudioSystem.getAudioInputStream(StandaloneResourceHolder.getURL("res/se/silence.wav"));
-			ByteBuffer bytes = ByteBuffer.wrap(IOUtils.toByteArray(ain));
-			silence = new SampleBuffer(ain.getFormat(), bytes);
-			ain.close();
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+		Arrays.fill(silence = new byte[128], (byte) 0x80);
 	}
-
+	
 	protected AudioFormat format;
 	protected SourceDataLine line;
 	protected SampleBuffer sample;
@@ -33,34 +27,35 @@ public class SampleBufferClippish extends Thread {
 	
 	protected Runnable emptiedTask;
 	
-	protected long writtenSamples;
-	protected long sampleOffset;
-	
-	protected boolean flushing;
+	protected Object sync = new Object();
 	
 	public SampleBufferClippish(AudioFormat format, SourceDataLine line) {
 		this.format = format;
 		this.line = line;
-		sbuf = new byte[22050];
-		setSample(silence, false);
+		sbuf = new byte[512];
 	}
 	
 	@Override
 	public void run() {
-		sampleOffset = line.getLongFramePosition();
 		try {
 			while(true) {
-				if(sample.remainingSamples() == 0 && line.getLongFramePosition() - sampleOffset >= writtenSamples - format.getSampleRate() / 100) {
-					if(sample != silence && emptiedTask != null	)
-						emptiedTask.run();
-					setSample(silence, false);
+				synchronized(sync) {
+					if(sample != null && sample.getBytes().remaining() == 0) {
+						if(emptiedTask != null	)
+							emptiedTask.run();
+						sample = null;
+					}
 				}
-				int count = Math.min(sample.getBytes().remaining(), sbuf.length);
-				sample.getBytes().get(sbuf, 0, count);
-				if(flushing)
-					line.flush();
-				line.write(sbuf, 0, count);
-				flushing = false;
+				Arrays.fill(sbuf, (byte) 0x80);
+				int count;
+				if(sample != null) {
+					if(sample.getBytes().position() == 0)
+						line.flush();
+					count = Math.min(sample.getBytes().remaining(), sbuf.length);
+					sample.getBytes().get(sbuf, 0, count);
+				} else
+					count = sbuf.length;
+				line.write(sbuf, 0, sbuf.length);
 				try {
 					Thread.sleep(Math.max(0, (long)(count * 1000L / format.getSampleRate()) - 50));
 				} catch (InterruptedException e) {
@@ -73,9 +68,6 @@ public class SampleBufferClippish extends Thread {
 
 	public void setSample(SampleBuffer sample, boolean flush) {
 		this.sample = new SampleBuffer(sample.getFormat(), sample.getBytes().duplicate());
-		flushing = flush;
-		writtenSamples = this.sample.getBytes().remaining();
-		sampleOffset = line.getLongFramePosition();
 		interrupt();
 	}
 	
