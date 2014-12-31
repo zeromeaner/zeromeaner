3836acb79b03c0f1f89e2a491f979ad39dd9a031
robin <robin.kirkman@gmail.com>
2014-12-31 02:58:56 -0500

    Add a button to browse issues too

--- zeromeaner-core/src/main/java/org/zeromeaner/gui/reskin/StandaloneFeedbackPanel.java
+++ zeromeaner-core/src/main/java/org/zeromeaner/gui/reskin/StandaloneFeedbackPanel.java
@@ -1,5 +1,6 @@
 package org.zeromeaner.gui.reskin;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -41,7 +42,6 @@ public class StandaloneFeedbackPanel extends JPanel {
 		JScrollPane fbs = new JScrollPane(fbta);
 		
 		JButton submitFeedback = new JButton(new AbstractAction("Submit Feedback Online") {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -53,9 +53,26 @@ public class StandaloneFeedbackPanel extends JPanel {
 		});
 		add(
 				submitFeedback, 
-				new GridBagConstraints(0, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(25,25,0,25), 0, 0));
+				new GridBagConstraints(0, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(25,25,0,25), 0, 0));
+
 		add(
 				fbs, 
 				new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(25,25,25,25), 0, 0));
+
+		JButton issues = new JButton(new AbstractAction("Browse Issues Online") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					URLs.open(new URL("http://jira.robindps.com/browse/ZRM/?selectedTab=com.atlassian.jira.jira-projects-plugin:summary-panel"));
+				} catch (Exception e1) {
+					throw new RuntimeException(e1);
+				}
+			}
+		});
+		add(
+				issues, 
+				new GridBagConstraints(0, 2, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0,25,25,25), 0, 0));
+
+		
 	}
 }
