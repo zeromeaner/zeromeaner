b64226b50e5b1fa33523b0173947b4ac870f4d2b
robin <robin.kirkman@gmail.com>
2014-12-31 03:40:55 -0500

    Fix issue when searching for absolute files

--- zeromeaner/src/main/java/org/zeromeaner/standalone/StandaloneResourceStreamHook.java
+++ zeromeaner/src/main/java/org/zeromeaner/standalone/StandaloneResourceStreamHook.java
@@ -15,7 +15,11 @@ public class StandaloneResourceStreamHook implements ResourceStreamHook {
 	@Override
 	public void addInputHandler(String resource, PrioritizedHandler<Callable<InputStream>> handlers) {
 		File FILE_BASE = new File(System.getProperty("user.dir"));
-		final File f = new File(FILE_BASE, resource);
+		final File f;
+		if(resource.startsWith(FILE_BASE.getPath()))
+			f = new File(resource);
+		else
+			f = new File(FILE_BASE, resource);
 		if(!f.canRead()) 
 			return;
 		Callable<InputStream> handler = new Callable<InputStream>() {
@@ -30,7 +34,11 @@ public class StandaloneResourceStreamHook implements ResourceStreamHook {
 	@Override
 	public void addOutputHandler(String resource, PrioritizedHandler<Callable<OutputStream>> handlers) {
 		File FILE_BASE = new File(System.getProperty("user.dir"));
-		final File f = new File(FILE_BASE, resource);
+		final File f;
+		if(resource.startsWith(FILE_BASE.getPath()))
+			f = new File(resource);
+		else
+			f = new File(FILE_BASE, resource);
 		f.getParentFile().mkdirs();
 		Callable<OutputStream> handler = new Callable<OutputStream>() {
 			@Override
@@ -44,7 +52,11 @@ public class StandaloneResourceStreamHook implements ResourceStreamHook {
 	@Override
 	public void addDeleteHandler(String resource, PrioritizedHandler<Callable<Boolean>> handlers) {
 		File FILE_BASE = new File(System.getProperty("user.dir"));
-		final File f = new File(FILE_BASE, resource);
+		final File f;
+		if(resource.startsWith(FILE_BASE.getPath()))
+			f = new File(resource);
+		else
+			f = new File(FILE_BASE, resource);
 		f.getParentFile().mkdirs();
 		Callable<Boolean> handler = new Callable<Boolean>() {
 			@Override
