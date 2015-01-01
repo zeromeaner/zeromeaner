7bbaeec6eef578dee6fe1690793f2935682e6298
robin <robin.kirkman@gmail.com>
2015-01-01 02:17:08 -0500

    Revert "Add localizations for video plugin.  Remove FPS option, since only 30FPS does anything anyway."
    This reverts commit bf6fed1cbf4fb1a8d17d0b4750a3e1f2042801e5.
    
    Conflicts:
    	zeromeaner-plugin-video-recording/src/main/java/org/zeromeaner/plugin/videorecording/StandaloneGamePanelHook.java
    	zeromeaner-plugin-video-recording/src/main/java/org/zeromeaner/plugin/videorecording/VideoRecordingConfigHook.java
    	zeromeaner-plugin-video-recording/src/main/resources/org/zeromeaner/plugin/videorecording/VideoRecordingConfigHook_en.properties

--- zeromeaner-plugin-video-recording/src/main/java/org/zeromeaner/plugin/videorecording/VideoRecordingConfigHook.java
+++ zeromeaner-plugin-video-recording/src/main/java/org/zeromeaner/plugin/videorecording/VideoRecordingConfigHook.java
@@ -17,28 +17,38 @@ public class VideoRecordingConfigHook implements Hook {
 	private static final Localization l = Localization.lz();
 	
 	protected JCheckBox enable = new JCheckBox(l.s("enable"));
+	protected JRadioButton fps30 = new JRadioButton(l.s("fps_30"));
+	protected JRadioButton fps60 = new JRadioButton(l.s("fps_60"));
 	
 	public VideoRecordingConfigHook() {
+		ButtonGroup g = new ButtonGroup();
+		g.add(fps30);
+		g.add(fps60);
+		
 		enable.setHorizontalAlignment(SwingConstants.CENTER);
+		fps30.setHorizontalAlignment(SwingConstants.CENTER);
+		fps60.setHorizontalAlignment(SwingConstants.CENTER);
 	}
 
 	@Override
 	public void createTabs(JTabbedPane tabs) {
 		JPanel panel = new JPanel(new GridLayout(0, 1));
 		panel.add(enable);
+		panel.add(fps30);
+		panel.add(fps60);
 		tabs.addTab(l.s("recording_tab_name"), panel);
 	}
 
 	@Override
 	public void saveConfiguration() {
 		VideoRecordingOptions.get().ENABLED.set(enable.isSelected());
-		VideoRecordingOptions.get().FPS.set(30);
+		VideoRecordingOptions.get().FPS.set(fps30.isSelected() ? 30 : 60);
 	}
 
 	@Override
 	public void loadConfiguration() {
 		enable.setSelected(VideoRecordingOptions.get().ENABLED.value());
-		VideoRecordingOptions.get().FPS.set(30);
+		(VideoRecordingOptions.get().FPS.value() == 30 ? fps30 : fps60).setSelected(true);
 	}
 
 }
